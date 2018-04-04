398fe57483cf14111fd049a9d1ca877fe04b9d83
Ryan Tablada <ryan.tablada@gmail.com>
2018-04-03 19:52:25 -0500

    chore: install tailwind css

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,9 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
-    // Add options here
+    'ember-cli-tailwind': {
+      buildTarget: 'app'
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
